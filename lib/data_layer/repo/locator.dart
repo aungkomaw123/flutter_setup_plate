f12d20c2f03b84115b6f1bf8162d121004742f00
aung ko maw <aungkomaw.123@gmail.com>
2023-07-18 13:33:47 +0630

    update

--- lib/data_layer/repo/locator.dart
+++ lib/data_layer/repo/locator.dart
@@ -1,1 +1,5 @@
+import 'package:get_it/get_it.dart';
 
+var getIt = GetIt.instance;
+
+void locator() {}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_setup_project/presentation_layer/screens/my_app.dart';
+import 'package:flutter_setup_project/data_layer/repo/locator.dart';
 import 'package:flutter_setup_project/presentation_layer/screens/splash_screen.dart';
 
 void main() {
+  locator();
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     theme: ThemeData(useMaterial3: true),
